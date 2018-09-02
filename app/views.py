bee611b6e12801f54a15e6ca7052c943a60a1c40
Evohmike <evohmike@gmail.com>
2018-09-02 08:43:37 +0300

    this renders the general news to the index html

--- app/views.py
+++ app/views.py
@@ -6,13 +6,13 @@ from .request import get_sources
 @app.route('/')
 def index():
 
-    #getting articles
-    source_news =get_sources('technology')
-    print(source_news)
+    #getting general news
+    general_news =get_sources('general')
+    print(general_news)
 
     title = 'Home-Welcome to news website'
 
-    return render_template('index.html',title = title, source_news=source_news)
+    return render_template('index.html',title = title, General=general_news)
 
 
 
