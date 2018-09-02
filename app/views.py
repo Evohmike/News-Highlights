435dc650f589d82d5a2b0909b5b5a8912fdb6d07
Evohmike <evohmike@gmail.com>
2018-09-02 11:42:12 +0300

    This added macro.html

--- app/views.py
+++ app/views.py
@@ -8,11 +8,21 @@ def index():
 
     #getting general news
     general_news =get_sources('general')
+    business_news =get_sources('business')
+    entertainment_news =get_sources('entertainment')
+    sports_news =get_sources('sports')
+    technology_news =get_sources('technology')
+    science_news =get_sources('science')
+    health_news =get_sources('health')
+
+
     print(general_news)
 
-    title = 'Home-Welcome to news website'
+    title = 'Home-Best News Upadate Site'
 
-    return render_template('index.html',title = title, General=general_news)
+    return render_template('index.html',title = title, General=general_news,Business=business_news,
+                           Entertainment=entertainment_news,Sports=sports_news,Technology=technology_news,
+                           Science=science_news,Health=health_news)
 
 
 
