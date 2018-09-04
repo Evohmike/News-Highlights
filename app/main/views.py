9cd8c3244ad9ae7dcfe8ef9ba13bc9b097978fdc
Evohmike <evohmike@gmail.com>
2018-09-04 10:26:38 +0300

    created a function that returns articles based on their sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,10 @@
 from flask import render_template, redirect, url_for, request
-from app import main
-from ..models import Sources
+from . import main
 from ..request import get_sources, get_articles
 
 
 @main.route('/')
 def index():
-
     # getting general news
 
     general_news = get_sources('general')
@@ -16,7 +14,6 @@ def index():
     technology_news = get_sources('technology')
     science_news = get_sources('science')
     health_news = get_sources('health')
-
 
     title = 'Home-Best News Update Site'
 
@@ -36,42 +33,4 @@ def articles(source_id, per_page):
     return render_template('articles.html', title=title, name=source_id, news=news_source)
 
 
-@main.route('/topheadlines&<int:per_page>')
-def headlines(per_page):
-    '''
-    Function that returns top headlines articles
-    '''
-    topheadlines_news = topheadlines(per_page)
-    title = 'Top Headlines'
-    return render_template('topheadlines.html', title=title, name='Top Headlines', news=topheadlines_news)
 
-
-@main.route('/everything&<int:per_page>')
-def all_news(per_page):
-    '''
-    Function that returns top headlines articles
-    '''
-    everything_news = everything(per_page)
-    title = 'All News'
-
-    search_articles = request.args.get('search_query')
-
-    if search_articles:
-        return redirect(url_for('main.search', topic=search_articles))
-    else:
-        return render_template('topheadlines.html', title=title, name='All News', news=everything_news)
-
-
-@main.route('/search/<topic>')
-def search(topic):
-    '''
-    function that returns the results of search request
-    '''
-    limit = 40
-    search_name = topic.split(" ")
-    search_name_format = "+".join(search_name)
-    search_every = search_everything(limit, search_name_format)
-
-    title = '{search_name_format} Results'
-
-    return render_template('search.html', title=title, news=search_every)
