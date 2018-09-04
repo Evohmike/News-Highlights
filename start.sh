f00b39927fb29ea6557ca389d3efb1e3fe00cb30
Evohmike <evohmike@gmail.com>
2018-09-04 10:44:50 +0300

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY='3ca37b6909144e9c9b06631ffb1d2bec'
-export SECRET_KEY='1234'
+export NEWS_API_KEY='31bebbcc6c64464a8aa87b249ce33341'
+#export SECRET_KEY='1234'
 
 python3.6 manage.py server
