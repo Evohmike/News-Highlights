3aa7afa8394282020788ceceac84c5af841f7ae8
Evohmike <evohmike@gmail.com>
2018-09-14 11:39:05 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='31bebbcc6c64464a8aa87b249ce33341'
-export SECRET_KEY='1234'
+export NEWS_API_KEY= 31bebbcc6c64464a8aa87b249ce33341
+export SECRET_KEY=1234
 
 python3.6 manage.py server
